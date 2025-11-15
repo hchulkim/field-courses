8c324f1ecdfc8d7b307b8e9b1a47e03c4cb6cc1f
Hyoungchul Kim <hchul.kim96@gmail.com>
2025-11-15 12:40:59 -0500

    setup nix/rix for rust analysis pset.

--- generate_env.R
+++ generate_env.R
@@ -4,10 +4,14 @@ path_default_nix <- here()
 
 library(rix)
 
-rix(date = "2025-09-09",
+rix(date = "2025-11-10",
     r_pkgs = c("languageserver", "rix", "here", "pacman", "data.table", "dplyr", "ggplot2", "fixest", "texreg", "kableExtra", "broom", "glue", "modelsummary"),
     system_pkgs = c("quarto", "git"),
     git_pkgs = NULL,
+    jl_conf = list(
+		   jl_version = "1.11",
+		   jl_pkgs = c("DataFrames", "CSV", "LinearAlgebra", "Statistics")
+		   ),
     ide = "none",
     project_path = path_default_nix,
     overwrite = TRUE,
---
+++ src/julia/rust_ddc.jl
@@ -0,0 +1,14 @@
+
+using DataFrames, CSV, LinearAlgebra, Statistics
+
+# set parameter values
+theta1 = 0.015
+theta2 = 0.001
+theta3 = 0.2
+R = 15.0
+beta = 0.95
+K = 90
+tol = 1e-10
+maxit = 20000
+
+# q1
